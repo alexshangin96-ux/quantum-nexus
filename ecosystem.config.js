7cd1be059b509d76e1668f6d15cd28f90ab04eef
alexshangin96-ux <alex.shangin96@gmail.com>
2025-10-25 05:47:02 +0700

    Add files via upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,15 @@
-cat > ecosystem.config.js << 
 module.exports = {
   apps: [{
-    name: 'quantum-nexus',
-    script: 'server.js',
+    name: 'quantum-nexus-bot',
+    script: 'bot.js',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true
+      BOT_TOKEN: '8426192106:AAGGlkfOYAhaQKPp-bcL-3oHXBE50tzAMog',
+      WEBAPP_URL: 'https://unlock-rent.online'
+    }
   }]
 };
