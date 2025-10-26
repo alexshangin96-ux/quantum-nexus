70a22d7293c3d77855b7e0a487cf4431313f6f3f
alexshangin96-ux <alexey.shangin96@gmail.com>
2025-10-26 22:03:48 +0700

    Import SupportTicket in web_server

--- web_server.py
+++ web_server.py
@@ -7,7 +7,7 @@ Server for Telegram Web App
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from database import get_db
-from models import User, MiningMachine, UserCard, Withdrawal
+from models import User, MiningMachine, UserCard, Withdrawal, SupportTicket
 from utils import calculate_offline_income
 from datetime import datetime
 from config import BASE_TAP_REWARD, ENERGY_COST_PER_TAP, MAX_ENERGY
